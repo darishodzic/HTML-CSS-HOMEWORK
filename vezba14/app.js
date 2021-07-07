b506f59f221e48f44914fb0b31fc14d80f1e5df5
Daris <daris.hodzic.dh@gmail.com>
2021-07-07 20:26:48 +0200

    vezba14

---
+++ vezba14/app.js
@@ -0,0 +1,17 @@
+function tab1(){
+    document.getElementById("tab1").style.display="block"
+    document.getElementById("tab2").style.display="none"
+    document.getElementById("tab3").style.display="none"
+
+}
+function tab2(){
+    document.getElementById("tab1").style.display="none"
+    document.getElementById("tab2").style.display="block"
+    document.getElementById("tab3").style.display="none"
+}
+
+function tab3(){
+    document.getElementById("tab1").style.display="none"
+    document.getElementById("tab2").style.display="none"
+    document.getElementById("tab3").style.display="block"
+}
