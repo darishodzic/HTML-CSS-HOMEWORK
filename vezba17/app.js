fab4c17f20a061c1bd494a1bc5ae218a2d605261
Daris <daris.hodzic.dh@gmail.com>
2021-07-28 15:40:50 +0200

    vezba17

---
+++ vezba17/app.js
@@ -0,0 +1,14 @@
+bodovi=50
+if(bodovi<=50){
+    console.log("Nedovoljan")
+}else if (bodovi<=63){
+    console.log("Dovoljan")
+}else if (bodovi<=76){
+    console.log("Dobar")
+}else if (bodovi<=89){
+    console.log("Vrlo dobar")
+}else if (bodovi<=100){
+    console.log("Odlican")
+}else{
+    console.log("Pogresan unos!")
+}
