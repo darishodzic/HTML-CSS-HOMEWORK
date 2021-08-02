41ec64e2862cc5a949b4b6d595c3ee8f0b9d3bee
Daris <daris.hodzic.dh@gmail.com>
2021-08-02 13:53:46 +0200

    updated vezba 19

--- vezba19/app.js
+++ vezba19/app.js
@@ -4,9 +4,16 @@
 //     }    
 // }
 
-var var1 = "w3resource"
-var var2 = ""
-for (let i = var1.length - 1; i>=0; i--) {
-    var2 += var1[i]
-}
-console.log(var2);
+// var var1 = "w3resource"
+// var var2 = ""
+// for (let i = var1.length - 1; i>=0; i--) {
+//     var2 += var1[i]
+// }
+// console.log(var2);
+
+var side1 = 5; 
+var side2 = 6; 
+var side3 = 7; 
+var s = (side1 + side2 + side3)/2;
+var area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+console.log(area);
