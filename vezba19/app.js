18c75129a8fdd02888589044c2865ac0ffa3e34a
Daris <daris.hodzic.dh@gmail.com>
2021-08-02 13:44:56 +0200

    vezba 19

---
+++ vezba19/app.js
@@ -0,0 +1,12 @@
+// for (let i = 0; i < 100; i++) {
+//     if (i%2==0) {
+//         console.log("Broj je :",i);
+//     }    
+// }
+
+var var1 = "w3resource"
+var var2 = ""
+for (let i = var1.length - 1; i>=0; i--) {
+    var2 += var1[i]
+}
+console.log(var2);
