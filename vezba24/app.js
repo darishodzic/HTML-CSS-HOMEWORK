df910c13e1ac0a1e4a3d26578c841ff02887e081
Daris <daris.hodzic.dh@gmail.com>
2021-08-16 13:56:46 +0200

    vezba24 i 25

---
+++ vezba24/app.js
@@ -0,0 +1,17 @@
+function unos(n,l) {
+    for (let i = 0; i < l; i++) {
+        n.push(parseInt(prompt("Uneiste element: ")))
+        
+    }
+    return n
+}
+mojNiz = []
+mojNiz = unos(mojNiz,4)
+
+function kvadriranje() {
+    for (let index = 0; index < array.length; index++) {
+        console.log(array[index],array[index]**2)
+        
+    }
+}
+kvadriranje()
